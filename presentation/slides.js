1d17cc48372a6783ab22236bb0659c48572e3053
Johnny Magrippis <j@magrippis.com>
2019-10-18 16:54:42 +0100

    Scaffold talk

---
+++ presentation/slides.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Slide, Text, Heading } from 'spectacle'
+import { MDXProvider } from '@mdx-js/tag'
+
+import components from './components'
+import theme, { darkComponents } from './theme'
+
+// DEFAULT LAYOUT
+
+export const DefaultSlide = ({ children, ...rest }) => (
+  <Slide {...rest}>
+    <MDXProvider components={darkComponents}>{children}</MDXProvider>
+  </Slide>
+)
+
+export const DarkSlide = ({ children, ...rest }) => (
+  <Slide bgColor="black" {...rest}>
+    <MDXProvider components={darkComponents}>{children}</MDXProvider>
+  </Slide>
+)
+
+// CODE LAYOUT
+
+export const CodeSlide = ({ children, ...rest }) => (
+  <Slide bgColor="#1d1f21" {...rest}>
+    <MDXProvider components={components}>{children}</MDXProvider>
+  </Slide>
+)
---
+++ presentation/theme/index.js
@@ -0,0 +1,99 @@
+import * as React from 'react'
+import { Slide, Text, Heading } from 'spectacle'
+
+import createTheme from '../../createTheme'
+
+const colors = {
+  primary: '#323639',
+  secondary: '#EFE9F4',
+  tertiary: '#26C485',
+  quaternary: '#A3E7FC',
+}
+
+const components = {
+  heading: {
+    h1: {
+      fontSize: '4rem',
+      textTransform: 'uppercase',
+    },
+    h2: {
+      fontSize: '3.5rem',
+      textTransform: 'uppercase',
+    },
+    h3: {
+      fontSize: '3rem',
+      textTransform: 'uppercase',
+    },
+    h4: {
+      fontSize: '2.5rem',
+      textTransform: 'uppercase',
+    },
+    h5: {
+      fontSize: '2rem',
+      textTransform: 'uppercase',
+    },
+    h6: {
+      fontSize: '1.5rem',
+      textTransform: 'uppercase',
+    },
+  },
+  codePane: {
+    fontSize: '2rem',
+  },
+}
+
+export const darkComponents = {
+  ...components,
+  h2: ({ children }) => (
+    <Heading size={2} textColor={theme.screen.colors.secondary}>
+      {children}
+    </Heading>
+  ),
+  h3: ({ children }) => (
+    <Heading size={3} textColor={theme.screen.colors.secondary}>
+      {children}
+    </Heading>
+  ),
+  h4: ({ children }) => (
+    <Heading size={4} textColor={theme.screen.colors.secondary}>
+      {children}
+    </Heading>
+  ),
+  h5: ({ children }) => (
+    <Heading size={5} textColor={theme.screen.colors.secondary}>
+      {children}
+    </Heading>
+  ),
+  h6: ({ children }) => (
+    <Heading size={6} textColor={theme.screen.colors.secondary}>
+      {children}
+    </Heading>
+  ),
+  p: ({ children }) => (
+    <Text textColor={theme.screen.colors.secondary}>{children}</Text>
+  ),
+}
+
+const theme = createTheme(
+  colors,
+  {
+    primary: 'Montserrat',
+    secondary: 'Helvetica',
+  },
+  {
+    progress: {
+      pacmanTop: {
+        background: colors.quaternary,
+      },
+      pacmanBottom: {
+        background: colors.quaternary,
+      },
+      point: {
+        borderColor: colors.quaternary,
+      },
+    },
+    components: darkComponents,
+  }
+)
+
+export default theme
